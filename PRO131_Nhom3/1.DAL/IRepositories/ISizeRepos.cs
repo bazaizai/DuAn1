dd3be921c6bba36993b6405692855cde75705751
UchiHahaha2k3 <gameno05tb@gmail.com>
2022-11-09 21:12:27 +0700

    xong het DAL roi

---
+++ PRO131_Nhom3/1.DAL/IRepositories/ISizeRepos.cs
@@ -0,0 +1,19 @@
+﻿using _1.DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.IRepositories
+{
+    public interface ISizeRepos
+    {
+        bool Add(Size obj);
+        bool Update(Size obj);
+
+        bool Delete(Size obj);
+        Size GetById(Guid id);
+        List<Size> GetSizes();
+    }
+}
---
+++ PRO131_Nhom3/1.DAL/Repositories/SizeRepos.cs
@@ -0,0 +1,81 @@
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.Repositories
+{
+    public class SizeRepos
+    {
+        public FpolyDBContext _dbContext;
+        public SizeRepos()
+        {
+            _dbContext = new FpolyDBContext();
+        }
+        public bool Add(Size obj)
+        {
+            obj.Id = Guid.NewGuid();
+            if (obj == null) return false;
+            try
+            {
+                _dbContext.Sizes.Add(obj);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+           
+        }
+
+        public bool Update(Size obj)
+        {
+            if (obj == null) return false;
+            try
+            {
+                var tempobj = _dbContext.Sizes.FirstOrDefault(x => x.Id == obj.Id);
+                tempobj.Ma = obj.Ma;
+                tempobj.Size1 = obj.Size1;
+                tempobj.Cm = obj.Cm;
+                tempobj.TrangThai = obj.TrangThai;
+                _dbContext.Sizes.Update(obj);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+        }
+
+        public bool Delete(Size obj)
+        {
+            if (obj == null) return false;
+            try
+            {
+                var tempobj = _dbContext.Sizes.FirstOrDefault(x => x.Id == obj.Id);
+                _dbContext.Remove(obj);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+           
+        }
+
+        public List<Size> GetSizes()
+        {
+            return _dbContext.Sizes.ToList();
+        }
+    }
+}
