b05e908f4deafcdf2b1c362b44364a993f4572ae
nugetrenkii <mr.zenner203@gmail.com>
2022-11-09 15:02:40 +0700

    code crud DAL repos va Irepos bang tich diem

--- PRO131_Nhom3/1.DAL/IRepositories/ITichDiemRepos.cs
+++ PRO131_Nhom3/1.DAL/IRepositories/ITichDiemRepos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1.DAL.DomainClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace _1.DAL.IRepositories
 {
     public interface ITichDiemRepos
     {
+        bool Add(TichDiem obj);
+        bool Update(TichDiem obj);
+        bool Delete(TichDiem obj);
+        TichDiem GetById(Guid id);
+        List<TichDiem> GetTichDiems();
+        List<TichDiem> GetAll();
     }
 }
--- PRO131_Nhom3/1.DAL/Repositories/TichDiemRepos.cs
+++ PRO131_Nhom3/1.DAL/Repositories/TichDiemRepos.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,54 @@ using System.Threading.Tasks;
 
 namespace _1.DAL.Repositories
 {
-    internal interface TichDiemRepos
+    public class TichDiemRepos : ITichDiemRepos
     {
+        private FpolyDBContext context;
+        private List<TichDiem> _lstTichDiem;
+        public bool Add(TichDiem obj)
+        {
+            if (obj == null) return false;
+            obj.Id = Guid.NewGuid();
+            context.TichDiems.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(TichDiem obj)
+        {
+            if (obj == null) return false;
+            var tempobj = context.TichDiems.FirstOrDefault(c => c.Id == obj.Id);
+            context.Remove(tempobj);
+            context.SaveChanges();
+            return true;
+        }
+
+        public List<TichDiem> GetAll()
+        {
+            return context.TichDiems.ToList();
+        }
+
+        public TichDiem GetById(Guid id)
+        {
+            if (id == Guid.Empty) return null;
+            return context.TichDiems.FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<TichDiem> GetTichDiems()
+        {
+            _lstTichDiem = context.TichDiems.ToList();
+            return _lstTichDiem;
+        }
+
+        public bool Update(TichDiem obj)
+        {
+            if (obj == null) return false;
+            var tempobj = context.TichDiems.FirstOrDefault(c => c.Id == obj.Id);
+            tempobj.SoDiem = obj.SoDiem;
+            tempobj.TrangThai = obj.TrangThai;
+            context.Update(tempobj);
+            context.SaveChanges();
+            return true;
+        }
     }
 }
