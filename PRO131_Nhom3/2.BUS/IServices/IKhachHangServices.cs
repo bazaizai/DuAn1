8b8ae29a927552186c14df817f1203b0a05374c3
nugetrenkii <mr.zenner203@gmail.com>
2022-11-10 13:49:05 +0700

    code xong BUS cua 4 bang KhachHang, LichSuTichDiem, TichDiem, UuDaiTichDiem

--- PRO131_Nhom3/2.BUS/IServices/IKhachHangServices.cs
+++ PRO131_Nhom3/2.BUS/IServices/IKhachHangServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2.BUS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace _2.BUS.IServices
 {
     public interface IKhachHangServices
     {
+        string Add(KhachHangView obj);
+        string Update(KhachHangView obj);
+        string Delete(KhachHangView obj);
+        KhachHangView GetByID(Guid id);
+        List<KhachHangView> Search(string obj);
+        List<KhachHangView> GetAll();
     }
 }
